70cc60a09d92af191080b14ed9aae43c3751a9c9
Mary-29 <masha_goncharova_2019@mail.ru>
2023-01-23 03:42:15 +0400

    feat:added adaptive menu

--- plants/js/index.js
+++ plants/js/index.js
@@ -1,3 +1,37 @@
 console.log(
   '1.Valid layout +10\n2.Semantic layout +20\n3.The layout corresponds to the layout +48\n4.css requirements +12\n5.Interactivity through css +20\n Score: 100'
 );
+
+
+const BurgerButton = document.querySelector('#burger-button')
+const NavBar = document.querySelector('nav')
+const CloseButton = document.querySelector('#close-button')
+
+function closeMenu() {
+  NavBar.style.display = 'none';
+  // NavBar.classList.toggle('active');
+  CloseButton.style.display = 'none';
+  BurgerButton.style.display = 'block';
+}
+
+function registerButton () {
+  BurgerButton.addEventListener('click', function () {
+    NavBar.style.display = 'block';
+    // NavBar.classList.toggle('active');
+    CloseButton.style.display = 'block';
+    BurgerButton.style.display = 'none';
+
+    const Navs = document.querySelectorAll('li .inika')
+
+    CloseButton.addEventListener('click', closeMenu);
+
+    for (let i = 0; i < Navs.length; i++) {
+      Navs[i].addEventListener('click', closeMenu);
+    }
+
+  })
+}
+
+document.addEventListener("resize", registerButton)
+
+onresize = registerButton
